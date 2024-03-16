f51143e444eeccefaf8f12a531d7eaf78ba6494e
huntermutimal <huntermutimal@mymacewan.ca>
2024-03-16 09:59:32 -0600

    Ability to click on Point Graphics

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -18,13 +18,9 @@ package com.mycompany.app;
 
 
 
-import com.esri.arcgisruntime.geometry.Point;
-import com.esri.arcgisruntime.geometry.PointCollection;
-import com.esri.arcgisruntime.geometry.Polygon;
-import com.esri.arcgisruntime.geometry.Polyline;
-import com.esri.arcgisruntime.geometry.SpatialReferences;
-import com.esri.arcgisruntime.mapping.view.Graphic;
-import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
+import com.esri.arcgisruntime.concurrent.ListenableFuture;
+import com.esri.arcgisruntime.geometry.*;
+import com.esri.arcgisruntime.mapping.view.*;
 import com.esri.arcgisruntime.symbology.SimpleFillSymbol;
 import com.esri.arcgisruntime.symbology.SimpleLineSymbol;
 import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol;
@@ -33,16 +29,15 @@ import com.esri.arcgisruntime.ArcGISRuntimeEnvironment;
 import com.esri.arcgisruntime.mapping.BasemapStyle;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Viewpoint;
-import com.esri.arcgisruntime.mapping.view.MapView;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Point2D;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -50,11 +45,14 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.List;
 
 public class App extends Application {
 
     private MapView mapView;
     private ArcGISMap map;
+
+    private ListenableFuture<IdentifyGraphicsOverlayResult> identifyGraphics;
 
     public static void main(String[] args) {
         Application.launch(args);
@@ -69,17 +67,17 @@ public class App extends Application {
         double screenSizeWidth = screenSize.getWidth();
         double screenSizeHeight = screenSize.getHeight() * 0.96;
 
-        // Load objects from FXML file into scenes and then into tabs
+        // Load objects from FXML file into scenes and then into tabs (this can be grouped into a function I think)
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/reports.fxml")); // Make sure to have / in front of file name
         Scene reportsScene = new Scene(fxmlLoader.load());
 
         FXMLLoader mapFxmlLoader = new FXMLLoader(App.class.getResource("/mapScreen.fxml"));
-        Scene otherMapScene = new Scene(mapFxmlLoader.load());
+        Scene mapScene = new Scene(mapFxmlLoader.load());
 
         FXMLLoader settingsFxmlLoader = new FXMLLoader(App.class.getResource("/settings.fxml"));
         Scene settingsScene = new Scene(settingsFxmlLoader.load());
 
-        Tab tab1 = new Tab("Map", otherMapScene.getRoot());
+        Tab tab1 = new Tab("Map", mapScene.getRoot());
         Tab tab2 = new Tab("Reports"  , reportsScene.getRoot());
         Tab tab3 = new Tab("Settings" , settingsScene.getRoot());
 
@@ -94,25 +92,38 @@ public class App extends Application {
         // create a MapView to display the map and add it to the stack pane
         mapView = new MapView();
         // Sets map style
-        map = new ArcGISMap(BasemapStyle.ARCGIS_STREETS);
+        map = new ArcGISMap(BasemapStyle.ARCGIS_IMAGERY);
         mapView.setMap(map);
 
         // Set Default location
         mapView.setViewpoint(new Viewpoint(53.5409, -113.5084, 122227.638572));
         // Set to StackPane inside tab
-        StackPane otherStackPane = (StackPane) otherMapScene.lookup("#mapPane"); // This is how to lookup elements
-        otherStackPane.getChildren().add(mapView);                                 // You set the ID under code in SceneBuilder
+        StackPane mapStackPane = (StackPane) mapScene.lookup("#mapPane"); // This is how to lookup elements
+        mapStackPane.getChildren().add(mapView);                                 // You set the ID under code in SceneBuilder
 
         // create a graphics overlay and add it to the map view
         GraphicsOverlay graphicsOverlay = new GraphicsOverlay();
         mapView.getGraphicsOverlays().add(graphicsOverlay);
 
-        // create a point geometry with a geo-coords
+        // create a point geometry with a geo-coords, Detail can be added to Point/Graphics via attribute dictionary
         Graphic pointGraphic = createPointGraphic(-113.6243, 53.5221);
+        pointGraphic.getAttributes().put("name", "West Edmonton Mall");
 
         // add the point graphic to the graphics overlay
         graphicsOverlay.getGraphics().add(pointGraphic);
 
+
+        mapView.setOnMouseClicked(mouseEvent -> {
+            if (mouseEvent.getButton() == MouseButton.PRIMARY && mouseEvent.isStillSincePress()) {
+                // create a point from location clicked
+                Point2D mapViewPoint = new Point2D(mouseEvent.getX(), mouseEvent.getY());
+
+                // identify graphics on the graphics overlay
+                identifyGraphics = mapView.identifyGraphicsOverlayAsync(graphicsOverlay, mapViewPoint, 5, false);
+
+                identifyGraphics.addDoneListener(() -> Platform.runLater(this::createGraphicDialog));
+            }
+        });
 
         stage.setScene(new Scene(root, screenSizeWidth, screenSizeHeight));
         stage.show();
@@ -129,6 +140,34 @@ public class App extends Application {
             mapView.dispose();
         }
     }
+
+
+    /**
+     * Indicates when a graphic is clicked by showing an Alert.
+     */
+    private void createGraphicDialog() {
+
+        try {
+            // get the list of graphics returned by identify
+            IdentifyGraphicsOverlayResult result = identifyGraphics.get();
+            List<Graphic> graphics = result.getGraphics();
+
+            if (!graphics.isEmpty()) {
+                // show an alert dialog box if a graphic was returned
+                var dialog = new Alert(Alert.AlertType.INFORMATION);
+                dialog.initOwner(mapView.getScene().getWindow());
+                dialog.setHeaderText(null);
+                dialog.setTitle("Information Dialog Sample");
+                // Should map Graphic name to school/data here to retrieve its details
+                dialog.setContentText("Clicked on " + graphics.get(0).getAttributes().get("name") + " graphic");
+                dialog.showAndWait();
+            }
+        } catch (Exception e) {
+            // on any error, display the stack trace
+            e.printStackTrace();
+        }
+    }
+
 
 
     public Graphic createPointGraphic(double latitude, double longitude){
