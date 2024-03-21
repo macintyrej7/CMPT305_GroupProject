7bc05abd470fc135f876aa6607a8b4f16accd3cb
huntermutimal <huntermutimal@mymacewan.ca>
2024-03-21 10:30:19 -0600

    Update Controller Name.

--- src/main/java/com/mycompany/app/MapScreenController.java
+++ src/main/java/com/mycompany/app/MapScreenController.java
@@ -20,7 +20,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Point2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseButton;
